dc35f926ab343717777b54db1e29754bc5dd0e65
kameshravada <ravadavenkatakamesh123@gmail.com>
2025-08-06 12:07:38 +0530

    modified

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+console.log("Vite configuration loaded");
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
